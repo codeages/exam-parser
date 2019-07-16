6abb88eeddb30745e53d8e7a18f84087abb8a9f4
zhongyunchang <zhongyunchang@yeah.net>
2019-07-16 15:56:59 +0800

    修改题目错误码处理

--- src/Parser/QuestionType/AbstractQuestion.php
+++ src/Parser/QuestionType/AbstractQuestion.php
@@ -17,4 +17,38 @@ abstract class AbstractQuestion
     const DEFAULT_DIFFICULTY = 'normal';
 
     abstract public function convert($questionLines);
+
+    protected function matchDifficulty(&$question, $line)
+    {
+        if (strpos(trim($line), self::DIFFICULTY_SIGNAL) === 0) {
+            $difficulty = str_replace(self::DIFFICULTY_SIGNAL, '', $line);
+            $difficultyCode = 'normal';
+            if (trim($difficulty) == '简单') {
+                $difficultyCode = 'simple';
+            }
+
+            if (trim($difficulty) == '一般') {
+                $difficultyCode = 'normal';
+            }
+
+            if (trim($difficulty) == '困难') {
+                $difficultyCode = 'difficulty';
+            }
+            $question['difficulty'] = $difficultyCode ? : self::DEFAULT_DIFFICULTY;
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function matchScore(&$question, $line)
+    {
+        if (strpos(trim($line), self::SCORE_SIGNAL) === 0) {
+            preg_match('/(([1-9]\d*\.\d*|0\.\d*[1-9]\d*)|[1-9]\d*)/', $line, $matches);
+            $question['score'] = isset($matches[0]) ? $matches[0] : self::DEFAULT_SCORE;
+            return true;
+        };
+
+        return false;
+    }
 }
--- src/Parser/QuestionType/Choice.php
+++ src/Parser/QuestionType/Choice.php
@@ -13,58 +13,62 @@ class Choice extends AbstractQuestion
         $answers = array();
         foreach ($questionLines as $line) {
             //处理选项
-            if (preg_match('/<#([A-Z])#>/', $line, $matches)) {
-                $question['options'][ord($matches[1])-65] = preg_replace('/<#([A-Z])#>/', '', $line);
+            if ($this->matchOptions($question, $line)) {
                 $stemStatus = true;
                 continue;
             }
             //处理答案
-            if (strpos(trim($line), self::ANSWER_SIGNAL) === 0) {
-                preg_match_all('/[A-Z]/', $line, $matches);
-                if ($matches) {
-                    foreach($matches[0] as $answer) {
-                        $answers[] = ord($answer)-65;
-                    }
-                }
-                $question['answers'] = $answers;
-                if (count($answers) > 1) {
-                    $question['type'] = 'choice';
-                }else {
-                    $question['type'] = 'single_choice';
-                }
+            if ($this->matchAnswers($question, $line)) {
+                $stemStatus = true;
                 continue;
             }
             //处理难度
-            if (strpos(trim($line), self::DIFFICULTY_SIGNAL) === 0) {
-                $difficulty = str_replace(self::DIFFICULTY_SIGNAL, '', $line);
-                $difficultyCode = 'normal';
-                if (trim($difficulty) == '简单') {
-                    $difficultyCode = 'simple';
-                }
-
-                if (trim($difficulty) == '一般') {
-                    $difficultyCode = 'normal';
-                }
-
-                if (trim($difficulty) == '困难') {
-                    $difficultyCode = 'difficulty';
-                }
-                $question['difficulty'] = $difficultyCode ? : self::DEFAULT_DIFFICULTY;
+            if ($this->matchDifficulty($question, $line)) {
+                $stemStatus = true;
                 continue;
             }
             //处理分数
-            if (strpos(trim($line), self::SCORE_SIGNAL) === 0) {
-                preg_match('/(([1-9]\d*\.\d*|0\.\d*[1-9]\d*)|[1-9]\d*)/', $line, $matches);
-                $question['score'] = isset($matches[0]) ? $matches[0] : self::DEFAULT_SCORE;
+            if ($this->matchScore($question, $line)) {
+                $stemStatus = true;
                 continue;
             };
 
             if (!$stemStatus) {
-                $question['stem'] .= PHP_EOL.$line;
+                $question['stem'] .= preg_replace('/^\d{0,5}(\.|、|。|\s)/','',$line).PHP_EOL;
             }
 
         }
         return $question;
     }
+
+    protected function matchOptions(&$question, $line)
+    {
+        if (preg_match('/<#([A-Z])#>/', $line, $matches)) {
+            $question['options'][ord($matches[1])-65] = preg_replace('/<#([A-Z])#>/', '', $line);
+            return true;
+        }
+        return false;
+    }
+
+    protected function matchAnswers(&$question, $line)
+    {
+        if (strpos(trim($line), self::ANSWER_SIGNAL) === 0) {
+            preg_match_all('/[A-Z]/', $line, $matches);
+            if ($matches) {
+                foreach($matches[0] as $answer) {
+                    $answers[] = ord($answer)-65;
+                }
+            }
+            $question['answers'] = $answers;
+            if (count($answers) > 1) {
+                $question['type'] = 'choice';
+            }else {
+                $question['type'] = 'single_choice';
+            }
+            return true;
+        }
+        return false;
+    }
+
     
 }
