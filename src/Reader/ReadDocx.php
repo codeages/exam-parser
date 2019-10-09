94da7e0f755de272ea6767db6f2813ef13179860
zhangyao <zhangyao@howzhi.com>
2019-10-09 11:07:33 +0800

    feat: #1009 docx图片解析添加pict类型解析

--- src/Reader/ReadDocx.php
+++ src/Reader/ReadDocx.php
@@ -185,8 +185,8 @@ class ReadDocx
             preg_match('/height:(.*?)pt;/', $style, $heightMatches);
 
             if (isset($rels[$imageId]) && isset($widthMatches[1]) && isset($heightMatches[1])) {
-                $width = (int) $widthMatches[1] * self::PT_PX;
-                $height = (int) $heightMatches[1] * self::PT_PX;
+                $width = (int) ($widthMatches[1] * self::PT_PX);
+                $height = (int) ($heightMatches[1] * self::PT_PX);
 
                 $file = $this->getZipResource($rels[$imageId]);
                 if ($file) {
